b2e6021591b412b557f99709bc00a1963c79cfef
OsniFilipo <osnifilipo@gmail.com>
2021-11-17 19:39:06 -0300

    aula 5 terminada

--- Ola_Mundo - Aula 5/lib/main.dart
+++ Ola_Mundo - Aula 5/lib/main.dart
@@ -1,8 +1,14 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // Aparece ele import por causa do runAPP
 
-import 'App_Widget.dart';
+main() { // main seria o primeiro metado para a criação do projeto
+  runApp(Container( //  Containet é um widget
+    child: Center(
+      child: Text(
+        'Flutterando',
+        textDirection: TextDirection.ltr, // Indica o que é um texto
+        style: TextStyle(color: Colors.white, fontSize: 50.0), // esta a parte para colocar a cor do projeto
+        ),
+      ),
+  ));
+} // Cuidado com as virgulas no final precisa esta tudo certo se abrio o um parendese tem que fechar
 
-main() {
-  runApp(AppWidget());
-}
