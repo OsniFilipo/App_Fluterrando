f35900d4ddea6396504dfcac4ce1d64a58226e18
OsniFilipo <osnifilipo@gmail.com>
2021-11-17 23:54:46 -0300

    Erro no código

--- Aulas do Flutterando/Aluna 6 + Código/Ola_Mundo - Aula 6/lib/main.dart
+++ Aulas do Flutterando/Aluna 6 + Código/Ola_Mundo - Aula 6/lib/main.dart
@@ -8,8 +8,8 @@ main() {
 class AppWidget extends StatelessWidget {
   final String title;
 
-  const AppWidget({Key key,this.title}) : super(key: key)
-
+  const AppWidget({ Key ,required this.title}) : super(key: key) // Na parte de key coloquei required  ou tirei a segunda key e  tirou o erro.
+  //required this.title tira o erro do this.title
   @override
   Widget build(BuildContext context) {
     return Container(
